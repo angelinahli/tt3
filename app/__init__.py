7064310062ac46b142fcda6ff3860c43b47f347c
Angelina Li <ali6@wellesley.edu>
2018-05-24 13:56:41 +0800

    some finishing touches

--- app/__init__.py
+++ app/__init__.py
@@ -8,11 +8,13 @@ description: initializes application
 """
 
 from flask import Flask
+from config import Config
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config.from_object(Config)
 login = LoginManager(app)
 login.login_view = "login"
 
--- config.py
+++ config.py
@@ -7,6 +7,3 @@ BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY", "very-secret-key-123")
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL",
-        "sqlite:///" + os.path.join(BASEDIR, "test.db"))
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
